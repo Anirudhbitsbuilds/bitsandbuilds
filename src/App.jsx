85b05802d363f247f31bffbaa6d9c1ea2bfd514e
Anirudhbitsbuilds <anirudhuppal100@gmail.com>
2025-07-08 18:16:32 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Bits&Builds</h1>
+      <p>Your one-stop shop for custom PCs and components.</p>
+    </div>
+  );
+}
+
+export default App;
